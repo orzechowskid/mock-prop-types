278a2700eb65f15ba229912ebd1aa0f7f3564265
Dan Orzechowski <orzechod@gmail.com>
2019-05-14 08:52:52 -0400

    noodlin'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+const PropTypes = require('prop-types');
 const serialize = require('serialize-javascript');
+
+function _serialize(items) {
+    console.log(`items:`, items);
+    try {
+        return serialize(items.map(function(item) {
+            return item.toString
+                ? item.toString()
+                : item;
+        }));
+    } catch (ex) {
+        console.log(`items:`, items);
+        return 'herpaderp';
+    }
+}
 
 /**
  * create a PropType of the given type
@@ -6,21 +21,42 @@ const serialize = require('serialize-javascript');
  * @return{Proxy}
  */
 function _getPropProxy(type) {
-    return new Proxy(function() { }, {
+    const name = `_PropTypes.${type}`;
+    const baseProp = PropTypes[type];
+    const requiredProp = baseProp.isRequired;
+    const requiredProxy = new Proxy(requiredProp, {
         get(target, prop) {
             switch (prop) {
                 case `name`:
-                    return type;
+                    return `${name} (required)`;
+                case Symbol.toPrimitive:
+                    return `_herp${name} (required)`;
+                case toString:
+                    return function() { return `_herp${name} (required)`; };
+                default:
+                    return baseProp[prop];
+            }
+        }
+    });
+    const p = new Proxy(function() { }, {
+        get(target, prop) {
+            switch (prop) {
+                case `name`:
+                    return name;
                 case `isRequired`:
-                    return `${type} (required)`;
-                case `toJSON`:
-                case `toString`:
-                    return function() { return type; };
+                    return requiredProxy;
+                case toString:
+                case Symbol.toPrimitive:
+                    return function() { return `_derp`; };
                 default:
                     return target[prop];
             }
         }
     });
+
+    p.toString = p[Symbol.toPrimitive] = function() { return name; };
+
+    return p;
 }
 
 /**
@@ -29,36 +65,53 @@ function _getPropProxy(type) {
  * @return{Function}
  */
 function _getFnPropProxy(type) {
-    const fn = function(...args) {
-        const key = serialize(args);
+    const name = `_PropTypes.${type}`;
+    const baseFnProp = PropTypes[type];
+    const p = new Proxy(function() { }, {
+        apply(target, thisArg, args) { // e.g. oneOf(...args)
+            const key = _serialize(args);
 
-        fn[key] = fn[key] || _getPropProxy(`${type}(${key})`);
+            if (p[key]) {
+                return p[key];
+            }
 
-        return fn[key];
-    };
+            p[key] = new Proxy(PropTypes[type].apply(thisArg, args), {
+                get(target, prop) {
+                    
+                    return target[prop];
+                }
+            });
+            p[key].toString = p[key][Symbol.toPrimitive] =
+                function() { return `balls`; };
 
-    return fn;
+            return p[key];
+        }
+    });
+
+    p.toString = p[Symbol.toPrimitive] = function() { return `ohhh damn`; };
+
+    return p;
 }
 
 const _exports = {
-    any: _getPropProxy(`PropTypes.any`),
-    array: _getPropProxy(`PropTypes.array`),
-    arrayOf: _getFnPropProxy(`PropTypes.arrayOf`),
-    bool: _getPropProxy(`PropTypes.bool`),
-    element: _getPropProxy(`PropTypes.element`),
-    elementType: _getPropProxy(`PropTypes.elementType`),
-    exact: _getFnPropProxy(`PropTypes.exact`),
-    func: _getPropProxy(`PropTypes.func`),
-    instanceOf: _getFnPropProxy(`PropTypes.instanceOf`),
-    node: _getPropProxy(`PropTypes.node`),
-    number: _getPropProxy(`PropTypes.number`),
-    object: _getPropProxy(`PropTypes.object`),
-    objectOf: _getFnPropProxy(`PropTypes.objectOf`),
-    oneOf: _getFnPropProxy(`PropTypes.oneOf`),
-    oneOfType: _getFnPropProxy(`PropTypes.oneOfType`),
-    shape: _getFnPropProxy(`PropTypes.shape`),
-    string: _getPropProxy(`PropTypes.string`),
-    symbol: _getPropProxy(`PropTypes.symbol`)
+    any: _getPropProxy(`any`),
+    array: _getPropProxy(`array`),
+    arrayOf: _getFnPropProxy(`arrayOf`),
+    bool: _getPropProxy(`bool`),
+    element: _getPropProxy(`element`),
+    elementType: _getPropProxy(`elementType`),
+    exact: _getFnPropProxy(`exact`),
+    func: _getPropProxy(`func`),
+    instanceOf: _getFnPropProxy(`instanceOf`),
+    node: _getPropProxy(`node`),
+    number: _getPropProxy(`number`),
+    object: _getPropProxy(`object`),
+    objectOf: _getFnPropProxy(`objectOf`),
+    oneOf: _getFnPropProxy(`oneOf`),
+    oneOfType: _getFnPropProxy(`oneOfType`),
+    shape: _getFnPropProxy(`shape`),
+    string: _getPropProxy(`string`),
+    symbol: _getPropProxy(`symbol`)
 };
 
 _exports.default = _exports;
